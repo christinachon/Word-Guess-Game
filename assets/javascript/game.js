bb2d544030f645f9a5db8bfb2795836f32abc880
Christina <christinachon90@gmail.com>
2019-06-05 14:13:55 -0700

    choose random word and input blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,13 @@ var words = ["link", "hyrule", "ganondorf", "kakariko"];
 
 //pick a random word from the words array
 var word = words[Math.floor(Math.random() * words.length)];
+
+//changes random word to blanks
+var answerArray = [];
+
+for(var i=0; i< word.length; i++){
+    answerArray[i]= "_ ";
+    }
+
+//displays the random word
+document.getElementById("blank").innerHTML = answerArray;
