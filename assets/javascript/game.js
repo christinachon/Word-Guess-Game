8f0b45b0886096b410a6f5c2f8ff3c3564a2582d
Christina <christinachon90@gmail.com>
2019-06-05 00:53:07 -0700

    added image and words array

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// my array of words
+
+var words = ["link", "hyrule", "ganondorf", "kakariko"];
+
+//pick a random word from the words array
+var word = words[Math.floor(Math.random() * words.length)];
