3344abd3146138fcfb06ae11238213dbb9357581
Christina <christinachon90@gmail.com>
2019-06-06 17:31:15 -0700

    added number of guesses, wins, losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 // my array of words
-
 var words = ["link", "hyrule", "ganondorf", "kakariko"];
 
 //pick a random word from the words array
@@ -7,11 +6,21 @@ var word = words[Math.floor(Math.random() * words.length)];
 
 //changes random word to blanks
 var answerArray = [];
-
 for(var i=0; i< word.length; i++){
     answerArray[i]= "_ ";
     }
 
-//displays the random word
+//displays the random word with no comma between
 document.getElementById("blank").innerHTML = (answerArray.join(""));
 
+// sets number of guesses left and displays
+var guessesLeft = 10;
+document.getElementById("guessesleft-text").innerHTML = guessesLeft;
+
+//sets wins
+var wins = 0;
+document.getElementById("wins-text").innerHTML = wins;
+
+//sets losses
+var lose = 0;
+document.getElementById("losses-text").innerHTML = lose;
